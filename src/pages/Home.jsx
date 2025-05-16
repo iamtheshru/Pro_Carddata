0760b63ce4a804a9a3e8c107ecb86f930db1d740
iamtheshru <s.j.patel.3110@gmail.com>
2025-05-16 22:27:01 +0530

    data

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,9 @@
 import products from "../data/products";
-// import { useCart } from "../context/CartContext.jsx";
+import { useCart } from "../context/CartContext.jsx";
 import { Link } from "react-router-dom";
 
 export default function Home() {
-    // const { addToCart } = useCart();
+    const { addToCart } = useCart();
 
     return (
         <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 p-4">
@@ -14,12 +14,12 @@ export default function Home() {
                         <h2 className="font-bold">{product.name}</h2>
                         <p>${product.price}</p>
                     </Link>
-                    {/* <button
+                    <button
                         onClick={() => addToCart(product)}
                         className="mt-2 bg-green-500 text-white px-3 py-1 rounded"
                     >
                         Add to Cart
-                    </button> */}
+                    </button>
                 </div>
             ))}
         </div>
